b26c93521c62586bb08c73a12e2b9d48a44b0c16
Robert Chen <robert.chen@thorntechnologies.com>
2016-02-07 11:29:57 -0500

    Adds menu helper and interact

---
+++ InteractiveSlideoutMenu/Interactor.swift
@@ -0,0 +1,14 @@
+//
+//  Interactor.swift
+//  InteractiveSlideoutMenu
+//
+//  Created by Robert Chen on 2/7/16.
+//  Copyright © 2016 Thorn Technologies, LLC. All rights reserved.
+//
+
+import UIKit
+
+class Interactor: UIPercentDrivenInteractiveTransition {
+    var hasStarted = false
+    var shouldFinish = false
+}
